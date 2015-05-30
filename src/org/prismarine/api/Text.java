3a7c9e9c804d306b2b93a46cff12d7631a2620a8
Nick Vernij <hello@nickforall.nl>
2015-05-30 21:12:48 +0200

    added text decoration

---
+++ src/org/prismarine/api/Text.java
@@ -0,0 +1,27 @@
+package org.prismarine.api;
+
+public enum Text {
+    BLACK,
+    DARK_BLUE,
+    DARK_GREEN,
+    DARK_AQUA,
+    DARK_RED,
+    DARK_PURPLE,
+    GOLD,
+    GRAY,
+    DARK_GRAY,
+    BLUE,
+    GREEN,
+    AQUA,
+    RED,
+    LIGHT_PURPLE,
+    YELLOW,
+    WHITE,
+
+    BOLD,
+    UNDERLINE,
+    ITALIC,
+    STRIKE,
+    RANDOM
+
+}
